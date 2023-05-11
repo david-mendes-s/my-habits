f8c0a0cc8f68fe0bd1220e06f1e377737dce6074
david-mendes-s <david.a.mendes.s@gmail.com>
2023-05-11 09:44:57 -0300

    reserve webhooks/usehabits

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app'
 import {SessionProvider as AuthProvider} from 'next-auth/react';
 import { Nunito, Poppins } from 'next/font/google';
-import { HabitsProvider } from '@/hooks/useHabits';
 
 export const nunito = Nunito({ subsets: ['latin'], variable: '--font-nunito', })
 
@@ -11,9 +10,8 @@ export const nunito = Nunito({ subsets: ['latin'], variable: '--font-nunito', })
 export default function App({ Component, pageProps }: AppProps) {
   return(
     <AuthProvider session={pageProps.session}>
-      <HabitsProvider>
+      
         <Component {...pageProps} />
-      </HabitsProvider>
      </AuthProvider>
   );  
 }
