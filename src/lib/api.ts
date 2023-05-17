66162c778b9aa4901d10ea76abb2725df1afe4b0
david-mendes-s <david.a.mendes.s@gmail.com>
2023-05-17 09:44:30 -0300

    axios config production/developer

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,21 @@
+
+// axiosConfig.js
 import axios from 'axios';
 
-export const api = axios.create({
-    baseURL: 'https://my-habits-alpha.vercel.app/api',
-    validateStatus: (status: number) => status >= 200 && status < 300,
+// Configurações comuns para todas as instâncias
+axios.defaults.validateStatus = (status) => status >= 200 && status < 300;
+
+// Configurações para o ambiente de produção
+const productionConfig = axios.create({
+  baseURL: 'https://my-habits-alpha.vercel.app/api', // Insira a URL da API de produção
 });
+
+// Configurações para o ambiente de desenvolvimento
+const developmentConfig = axios.create({
+  baseURL: 'http://localhost:3000/api', // Insira a URL da API de desenvolvimento
+});
+
+// Exporta a instância correta dependendo do ambiente
+export const api = process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig;
+
+
