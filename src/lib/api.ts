65d19c709d4f0ccb5a59aef89a9702b3097ef342
david-mendes-s <david.a.mendes.s@gmail.com>
2023-05-16 22:51:03 -0300

    producion

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://my-habits-8sr0qresf-david-mendes-s.vercel.app/api',
+    baseURL: 'my-habits-alpha.vercel.app/api',
     validateStatus: (status: number) => status >= 200 && status < 300,
 });
