afc2c629d085bbb441fe54f4f41ad0666d734b32
david-mendes-s <david.a.mendes.s@gmail.com>
2023-06-06 00:37:37 -0300

    next-config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,19 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com']
   },
-  
+  async headers() {
+    return [
+      {
+        source: "/about",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate=59",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 
